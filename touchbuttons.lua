5f1c99efe5debc558a4a8c05edfddd430690531e
InfarcT69 <88346770+InfarcT69@users.noreply.github.com>
2023-07-11 16:35:37 -0600

    Add files via upload

--- touchbuttons.lua
+++ touchbuttons.lua
@@ -1,13 +1,15 @@
 local touchbuttons = {}
 touchbuttons.__index = touchbuttons
 
-function touchbuttons:newRectangleButton(x, y, w, h)
+function touchbuttons:newRectangleButton(x, y, w, h, buttonType)
   local self = setmetatable({}, touchbuttons)
   self.x = x
   self.y = y
   self.w = w
   self.h = h
   self.isPressed = false
+  self.type = buttonType
+  if self.type ~= 'mouse' and self.type ~= 'touch' then error(self.type..' is not an existing type of button', 2) end
   return self
 end
 
@@ -45,23 +47,37 @@ function touchbuttons:setDimensions(w, h)
   self.h = h
 end
 
-function touchbuttons:checkPressed()
-  local touches = love.touch.getTouches()
+function touchbuttons:checkPressed(mouseButton)
   local isButtonPressed = false
-  for i,v in ipairs(touches) do
-    local touchX, touchY = love.touch.getPosition(v)
-      if touchX < self.x + self.w and
-         touchX + 10 > self.x and
-         touchY < self.y + self.h and
-         touchY + 10 > self.y and 
-         love.mouse.isDown(1) then
-        isButtonPressed = true
-        break
-      else
-        isButtonPressed = false
-        
-      end 
+  --touchButton
+  if self.type == 'touch' then
+    local touches = love.touch.getTouches()
+    for i,v in ipairs(touches) do
+     local touchX, touchY = love.touch.getPosition(v)
+     if touchX <= self.x + self.w and
+     touchX + 10 >= self.x and
+     touchY <= self.y + self.h and
+     touchY + 10 >= self.y then
+       isButtonPressed = true
+       break
+     else
+       isButtonPressed = false
+     end 
+    end
+  --MouseClickButton
+  elseif self.type == 'mouse' then
+    mouseX, mouseY = love.mouse.getPosition()
+    if mouseX <= self.x + self.w and
+    mouseX + 10 >= self.x and
+    mouseY <= self.y + self.h and
+    mouseY + 10 >= self.y and
+    love.mouse.isDown(mouseButton) then
+      isButtonPressed = true
+    else
+      isButtonPressed = false
+    end 
   end
+  --isPressed
   if isButtonPressed == true then
     self.isPressed = true
   elseif isButtonPressed == false then
